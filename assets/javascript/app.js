de701c969f2d7f0d81f9518dcb22fbfb254ed23d
Matthew Vanley <matt@vibrantsitedesign.com>
2019-03-19 13:10:57 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,7 @@
 //Create a Start button to iniate game
 
 //Once button is pressed the first question appears along with a 30 second timer
-
-
-
+//Questions
 function Quiz(questions) {
     this.score = 0;
     this.questions = questions;
@@ -22,6 +20,8 @@ Quiz.prototype.guess = function(answer) {
         this.score++;
     }
 }
+
+
 function populate() {
     if(quiz.isEnded()) {
         showScores();
@@ -29,19 +29,30 @@ function populate() {
     else {
         var element = document.getElementById("question");
         element.innerHTML = quiz.getQuestionIndex().text;
-
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i< choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+    }
+function guess(id, guess){
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
     }
 }
 
-function Question(text, choices, answer) {
-    this.text=text;
-    this.choices=choices;
-    this.answer=amswer;
+};
+
+function showScores() {
+    var gameOverHtml = "<h1>Result</h1>";
+        gameOverHtml += "<h2 id= 'score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHtml;
 }
-Question.prototype.correctAnswer = function(choice) {
-    return choice === this.answer;
-}
-//Questions
+
 var questions = [
     new Question("Who was the MVP of Super Bowl 32?", ["Peyton Manning", "John Elway", "Rod Smith", "Terrell Davis"], "Terrell Davis"),
     new Question("Who is the Broncos all time leading receiver?", ["Ed McCaffrey", "Demarious Thomas", "Rod Smith", "Shannon Sharpe"], "Rod Smith"),
@@ -53,6 +64,19 @@ var questions = [
 var quiz = new Quiz(questions);
 
 populate();
+console.log(populate)
+
+
+
+function Question(text, choices, answer) {
+    this.text=text;
+    this.choices=choices;
+    this.answer=answer;
+}
+Question.prototype.correctAnswer = function(choice) {
+    return choice === this.answer;
+}
+
 //If user guesses correctly then CORRECT pops up along with a gif for 3 seconds
 
 //If user guesses incorrectly then WRONG pops up along with a gif for 3 seconds
